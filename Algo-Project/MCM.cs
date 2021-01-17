61566a5604ed729cc56d34f9b6474d43c5bc59f6
Uzair <uzair_mughal20@yahoo.com>
2021-01-17 13:56:22 +0500

    Update

---
+++ Algo-Project/MCM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Algo_Project
+{
+    public partial class MCM : Form
+    {
+        private string filename;
+        private string str1;
+        private string str2;
+        public int[] cost;
+        public int[] dense;
+
+        public MCM(string temp)
+        {
+            filename = temp;
+            InitializeComponent();
+        }
+
+        private void ReadFile()
+        {
+            string[] lines = File.ReadAllLines(filename);
+            str1 = lines[0];
+            str2 = lines[1];
+        }
+
+        private void Algo()
+        {
+
+        }
+    }
+}
