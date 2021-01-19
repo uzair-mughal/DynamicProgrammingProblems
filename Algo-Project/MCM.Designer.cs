fcc13c6d122a91905394433e66b42906f115ddf8
Muzammil <muzammilsiddiq@gmail.com>
2021-01-19 19:57:23 +0500

    mcm_complete

--- Algo-Project/MCM.Designer.cs
+++ Algo-Project/MCM.Designer.cs
@@ -31,6 +31,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.back_button = new System.Windows.Forms.Button();
+            this.parenthesisTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button3
@@ -41,6 +43,7 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "Show Dense Matrix";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -60,12 +63,31 @@
             this.back_button.TabIndex = 3;
             this.back_button.Text = "Back";
             this.back_button.UseVisualStyleBackColor = true;
+            this.back_button.Click += new System.EventHandler(this.back_button_Click);
+            // 
+            // parenthesisTextBox
+            // 
+            this.parenthesisTextBox.Location = new System.Drawing.Point(126, 12);
+            this.parenthesisTextBox.Name = "parenthesisTextBox";
+            this.parenthesisTextBox.Size = new System.Drawing.Size(419, 20);
+            this.parenthesisTextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Parenthesis:";
             // 
             // MCM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.parenthesisTextBox);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.back_button);
@@ -73,6 +95,7 @@
             this.Text = "MCM";
             this.Load += new System.EventHandler(this.MCM_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +104,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button back_button;
+        private System.Windows.Forms.TextBox parenthesisTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Algo-Project/MCM.cs
+++ Algo-Project/MCM.cs
@@ -13,12 +13,13 @@ namespace Algo_Project
 {
     public partial class MCM : Form
     {
+        private string parent_str;
         private string filename;
         private string str1;
         private int left;
         private int right;
         public int[] dims;
-        public long[,] lookup;
+        public int[,] lookup;
         public int[,] dense;
 
         public MCM(string temp)
@@ -50,53 +51,93 @@ namespace Algo_Project
             }
         }
 
-        long MatrixChainMultiplication(int[] dims, int i, int j)
+        //long MatrixChainMultiplication(int[] dims, int i, int j)
+        //{
+        //    // base case: one matrix
+        //    if (j <= i + 1)
+        //        return 0;
+
+        //    // stores minimum number of scalar multiplications (i.e., cost)
+        //    // needed to compute the matrix M[i+1]...M[j] = M[i..j]
+        //    long min = 100000000000;
+        //    int k_min = 10000;
+
+        //    // if sub-problem is seen for the first time, solve it and
+        //    // store its result in a lookup table
+        //    if (lookup[i, j] == 0)
+        //    {
+        //        // take the minimum over each possible position at which the
+        //        // sequence of matrices can be split
+
+        //        /*
+        //            (M[i+1]) x (M[i+2]..................M[j])
+        //            (M[i+1]M[i+2]) x (M[i+3.............M[j])
+        //            ...
+        //            ...
+        //            (M[i+1]M[i+2]............M[j-1]) x (M[j])
+        //        */
+
+        //        for (int k = i + 1; k <= j - 1; k++)
+        //        {
+        //            // recur for M[i+1]..M[k] to get a i x k matrix
+        //            long cost = MatrixChainMultiplication(dims, i, k);
+
+        //            // recur for M[k+1]..M[j] to get a k x j matrix
+        //            cost += MatrixChainMultiplication(dims, k, j);
+
+        //            // cost to multiply two (i x k) and (k x j) matrix
+        //            cost += dims[i] * dims[k] * dims[j];
+
+        //            if (cost < min)
+        //                k_min = k;
+        //            min = cost;
+        //        }
+        //        lookup[i, j] = min;
+        //        dense[i, j] = k_min;
+        //    }
+
+        //    // return min cost to multiply M[j+1]..M[j]
+        //    return lookup[i, j];
+        //}
+
+        private void printParenthesis(int j, int i)
         {
-            // base case: one matrix
-            if (j <= i + 1)
-                return 0;
-
-            // stores minimum number of scalar multiplications (i.e., cost)
-            // needed to compute the matrix M[i+1]...M[j] = M[i..j]
-            long min = 100000000000;
-            int k_min = 10000;
-
-            // if sub-problem is seen for the first time, solve it and
-            // store its result in a lookup table
-            if (lookup[i, j] == 0)
+            if (j == i)
             {
-                // take the minimum over each possible position at which the
-                // sequence of matrices can be split
-
-                /*
-                    (M[i+1]) x (M[i+2]..................M[j])
-                    (M[i+1]M[i+2]) x (M[i+3.............M[j])
-                    ...
-                    ...
-                    (M[i+1]M[i+2]............M[j-1]) x (M[j])
-                */
-
-                for (int k = i + 1; k <= j - 1; k++)
-                {
-                    // recur for M[i+1]..M[k] to get a i x k matrix
-                    long cost = MatrixChainMultiplication(dims, i, k);
-
-                    // recur for M[k+1]..M[j] to get a k x j matrix
-                    cost += MatrixChainMultiplication(dims, k, j);
-
-                    // cost to multiply two (i x k) and (k x j) matrix
-                    cost += dims[i] * dims[k] * dims[j];
-
-                    if (cost < min)
-                        k_min = k;
-                    min = cost;
-                }
-                lookup[i, j] = min;
-                dense[i, j] = k_min;
+                parent_str += "A" + j.ToString() + " ";
+                return;
             }
+            else
+            {
+                parent_str += "( ";
+                printParenthesis(dense[i, j] - 1, i);
+                printParenthesis(j, dense[i, j]);
+                parent_str += ") ";
+            }
+        }
 
-            // return min cost to multiply M[j+1]..M[j]
-            return lookup[i, j];
+        private void matrixChainOrder(int[] dims, int n) {
+
+            for (int l = 2; l < n + 1; l++) 
+            {
+                for (int i = 0; i < n - l + 1; i++)
+                {
+                    int j = i + l - 1;
+
+                    lookup[i, j] = Int32.MaxValue;
+
+                    for (int k = i; k < j; k++)
+                    {
+                        int q = lookup[i, k] + lookup[k + 1, j] + (dims[i] * dims[k + 1] * dims[j + 1]);
+                        if (q < lookup[i, j])
+                        {
+                            lookup[i, j] = q;
+
+                            dense[i, j] = k + 1;
+                        }
+                    }
+                }
+            }
         }
 
         private void Algo()
@@ -104,19 +145,17 @@ namespace Algo_Project
 
             // Matrix M[i] has dimension dims[i-1] x dims[i] for i = 1..n
             // input is 10 x 30 matrix, 30 x 5 matrix, 5 x 60 matrix
-            int n = dims.Length;
-            lookup = new long[n, n];
+            int n = dims.Length - 1;
+            //string dims_string = "";
+
+            lookup = new int[n, n];
             dense = new int[n, n];
-            long min_cost = MatrixChainMultiplication(dims, 0, n - 1);
+            parent_str = "";
 
-            string dims_string = "";
+            matrixChainOrder(dims, n);
+            printParenthesis(n - 1, 0);
 
-            for (int i = 0; i < dims.Length; i++)
-            {
-                dims_string += dims[i].ToString() + " ";
-            }
-            
-            MessageBox.Show(dims_string + "\n" + min_cost.ToString());
+            parenthesisTextBox.Text = parent_str;
         }
 
         private void Load_String()
@@ -125,7 +164,7 @@ namespace Algo_Project
             left = 60;
             right = 135;
 
-            for (int i = 0; i < len; i++)
+            for (int i = 1; i < len; i++)
             {
                 TextBox textBox1 = new TextBox();
                 this.Controls.Add(textBox1);
@@ -143,7 +182,7 @@ namespace Algo_Project
 
             left = 10;
             right = 173;
-            for (int i = 0; i < len; i++)
+            for (int i = 1; i < len; i++)
             {
                 TextBox textBox1 = new TextBox();
                 this.Controls.Add(textBox1);
@@ -160,15 +199,15 @@ namespace Algo_Project
             }
         }
 
-        private void Load_Table()
+        private void Load_Table_Cost()
         {
 
             left = 60;
             right = 173;
 
-            for (int i = 0; i < dims.Length; i++)
+            for (int i = 0; i < dims.Length - 1; i++)
             {
-                for (int j = 0; j < dims.Length; j++)
+                for (int j = 0; j < dims.Length - 1; j++)
                 {
 
                     TextBox textBox1 = new TextBox();
@@ -194,17 +233,65 @@ namespace Algo_Project
             back_button.Location = new Point(left, right);
         }
 
+        private void Load_Table_Dense()
+        {
+
+            left = 60;
+            right = 173;
+
+            for (int i = 0; i < dims.Length - 1; i++)
+            {
+                for (int j = 0; j < dims.Length - 1; j++)
+                {
+
+                    TextBox textBox1 = new TextBox();
+                    this.Controls.Add(textBox1);
+                    textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+                    textBox1.ForeColor = System.Drawing.SystemColors.Info;
+                    textBox1.Location = new System.Drawing.Point(left, right);
+                    textBox1.Name = "textBox1";
+                    textBox1.Size = new System.Drawing.Size(44, 32);
+                    textBox1.TabIndex = 3;
+                    textBox1.Text = dense[i, j].ToString();
+                    textBox1.TextAlign = HorizontalAlignment.Center;
+                    textBox1.ReadOnly = true;
+                    left = left + 50;
+
+                }
+
+                right = right + 38;
+                left = 60;
+            }
+
+            left = 10;
+            back_button.Location = new Point(left, right);
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
             this.AutoScroll = true;
             Load_String();
-            Load_Table();
+            Load_Table_Cost();
         }
 
         private void MCM_Load(object sender, EventArgs e)
         {
             ReadFile();
             Algo();
+        }
+
+        private void back_button_Click(object sender, EventArgs e)
+        {
+            Main_Panel obj = new Main_Panel();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.AutoScroll = true;
+            Load_String();
+            Load_Table_Dense();
         }
     }
 }
